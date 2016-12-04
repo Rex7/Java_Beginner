5adf2e06cd5bc997c754f92d74871775083c568b
Rex7 <regischarles7@gmail.com>
2016-12-04 16:56:26 +0530

    hackerrank

---
+++ hackerrank/ExceptionHandling.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Regis
+ */
+class MyCalculator{
+    
+    public int power(int n,int p) throws Exception{
+        
+    
+        
+        if(n<0 || p<0){
+            throw new   Exception(" n and p should be non-negative");
+        }
+        return (int)Math.pow(n, p);
+   
+}
+}
+class Solution {
+
+ public static void main(String[] args) {
+  Scanner in = new Scanner(System.in);
+
+  while ( in .hasNextInt()) {
+   int n = in .nextInt();
+   int p = in .nextInt();
+   MyCalculator my_calculator = new MyCalculator();
+   try {
+    System.out.println(my_calculator.power(n, p));
+   } catch (Exception e) {
+    System.out.println(e);
+   }
+  }
+ }
+}
---
+++ hackerrank/StringFormatti.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Regis
+ */
+public class StringFormatti {
+    
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+             
+            }
+            System.out.println("================================");
+
+    }
+    
+}
